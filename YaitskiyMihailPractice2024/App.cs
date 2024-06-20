4345d341c62a4a6417339fcae57799bd876f5c8f
Mold <Mold@DESKTOP-GVLEQK1>
2024-06-20 13:39:54 +0500

    Add project files.

---
+++ YaitskiyMihailPractice2024/App.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace Yaitskiy_Mihail_Practice
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+
+    internal class App : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel("PillowCreater");
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+            PushButtonData buttonData = new PushButtonData("Bleh", "Bleeeh", thisAssemblyPath, "Yaitskiy_Mihail_Practice.BreakColumnsCommand");
+            PushButton pushbutton = ribbonPanel.AddItem(buttonData) as PushButton;
+            
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+
+        return Result.Succeeded; 
+        }
+
+       
+    }
+    
+    
+    
+ }
+
---
+++ YaitskiyMihailPractice2024/BreakColumnsCommand.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yaitskiy_Mihail_Practice
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+
+    internal class BreakColumnsCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("PillonCutter", " ▲" + "\n" +" ▲ ▲");
+            return Result.Succeeded;
+        }
+    }
+}
